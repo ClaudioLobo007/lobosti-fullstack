d9c9c9443a04c3f1479d0a5e6c4946aa09877dfc
ClaudioLobo007 <claudio.lobo@lobosti.com.br>
2025-08-09 12:44:12 -0300

    Feat: Alteração de Permissão PATCH

--- Servidor/server.js
+++ Servidor/server.js
@@ -18,8 +18,8 @@ const announcementRoutes = require('./routes/announcements');
 // 2. MIDDLEWARES GERAIS
 app.use(cors({
     origin: '*', // Permite qualquer origem (para desenvolvimento/teste)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
